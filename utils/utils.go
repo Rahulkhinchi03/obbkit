3c6dedc20e1010d0bde7610d55377302f46643d9
Daniel <adeboyed93@gmail.com>
2022-08-04 02:34:50 +0300

    added utils functionality

---
+++ utils/utils.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	var repo = "git@github.com:Onboardbase/Reactjs-Starterkit.git"
+	if err := os.Mkdir(name, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+	os.Chdir(name)
+	str, err := os.Getwd()
+	fmt.Printf("Project Directory: %v\n", str)
+	fmt.Printf("Possible error: %v\n", err)
+	cmd := exec.Command("git", "clone", repo, ".")
+	cmd.Run()
+	exec.Command("code", ".").Run()
+	fmt.Fprintln(os.Stdout, colorGreen, "Your reactjs project has been successfully created with title name of ->>", input)
+}
